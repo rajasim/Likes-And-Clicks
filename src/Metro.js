2b6bc793b43a8b0ad4a7874b03f43ff7e7346062
Anis Mahat <anismahat756.am1@gmail.com>
2025-05-12 12:19:53 +0530

    changes

--- src/Metro.js
+++ src/Metro.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Homepage.css";
 
 function Metro() {
+  const herotextref = useRef();
+
+  useEffect(() => {
+    if (herotextref.current) {
+      herotextref.current.style.transform = "translateX(0px)";
+      herotextref.current.style.transition = "transform 2s linear";
+    }
+  }, []);
   return (
-    <div 
+    <div
       className="homepage"
-      style={{ 
+      style={{
         backgroundImage: "url('/ho.jpeg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "105vh",
         width: "98%",
         position: "relative",
-        color: "white"
+        color: "white",
       }}
     >
       <div className="overlay"></div>
@@ -20,21 +28,33 @@ function Metro() {
       <nav className="navbar">
         <div className="logo">LIKES & CLICKS</div>
         <ul className="nav-links">
-          <li><b>Home</b></li>
-          <li><b>About</b></li>
-          <li><b>Blocks</b></li>
-          
-          <li><b>Contact</b></li>
-          
+          <li>
+            <b>Home</b>
+          </li>
+          <li>
+            <b>About</b>
+          </li>
+          <li>
+            <b>Blocks</b>
+          </li>
+
+          <li>
+            <b>Contact</b>
+          </li>
         </ul>
       </nav>
 
       <div className="hero-content">
-        <div className="hero-text">
-          <h1>Likes & Clicks <br />Driving Profit to your Doors</h1>
+        <div className="hero-text" ref={herotextref}>
+          <h1>
+            Likes & Clicks <br />
+            Driving Profit to your Doors
+          </h1>
           <p>
-            <strong>Likes & Clicks</strong> Likes & Clicks is a dynamic marketing and web development company dedicated to amplifying your online presence and driving tangible results.
-             <strong>Likes & Clickss</strong>.
+            <strong>Likes & Clicks</strong> Likes & Clicks is a dynamic
+            marketing and web development company dedicated to amplifying your
+            online presence and driving tangible results.
+            <strong>Likes & Clickss</strong>.
           </p>
           <button className="get-started-btn">Get Started</button>
         </div>
